9db976a3b19de816669a9825c8d0d554cbc59be2
Dan Hudlow <dhudlow@us.ibm.com>
2023-11-11 07:50:20 -0600

    first

---
+++ index.js
@@ -0,0 +1,94 @@
+const adjective = [
+  "Scandanavian",
+  "Polish",
+  "Czechian",
+  "Austrian",
+  "Slovenian",
+  "Croatian",
+  "Bosnian",
+  "Albanian",
+  "Serbian",
+  "Bulgarian",
+  "Romanian",
+  "Slovakian",
+  "Lithuanian",
+  "Latvian",
+  "Estonian",
+  "Russian"
+];
+
+const muscle = [
+  "hamstring",
+  "pectoral",
+  "chest",
+  "shoulder",
+  "calf",
+  "thigh",
+  "glute",
+  "leg",
+  "core",
+  "abdominal",
+  "quadricep",
+  "bicep",
+  "tricep",
+  "arm",
+  "back",
+  "spine",
+  "knee",
+  "elbow",
+  "deltoid",
+  "toe",
+  "pinkie",
+  "finger",
+  "thumb",
+  "wrist",
+  "grip",
+  "ankle",
+  "foot",
+  "palm",
+  "hand",
+  "shin",
+  "neck",
+  "cranial",
+  "pelvic",
+  "hip",
+  "chin",
+  "knuckle"
+];
+
+const motion = [
+  "hinge",
+  "lift",
+  "stretch",
+  "cross",
+  "raise",
+  "press",
+  "crunch",
+  "roll",
+  "twist",
+  "curl",
+  "row",
+  "squat",
+  "fly",
+  "lunge",
+  "push",
+  "pull",
+  "rotation",
+  "squeeze",
+  "dip",
+  "drop",
+  "slide",
+  "lower",
+  "kneel",
+  "bend",
+  "arch",
+  "flex"
+];
+
+function random(array) {
+  return array[Math.floor(Math.random()*array.length)];
+}
+
+function generate() {
+  return `${random(adjective)} ${random(muscle)} ${random(motion)}`
+}
